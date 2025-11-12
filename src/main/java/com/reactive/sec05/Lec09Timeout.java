aa0e176c51d99a7b8a921d30646bcc7079690791
yeganefard <aliyeganefard@gamil.com>
2025-11-12 20:26:15 +0330

    sec05

--- src/main/java/com/reactive/sec05/Lec09Timeout.java
+++ src/main/java/com/reactive/sec05/Lec09Timeout.java
@@ -1,28 +1,35 @@
 package com.reactive.sec05;
 
 import com.reactive.common.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
 public class Lec09Timeout {
+
+    private static final Logger log = LoggerFactory.getLogger(Lec09Timeout.class);
+
     public static void main(String[] args) {
-        getProductName()
-                .timeout(Duration.ofSeconds(1), fallback())
-//                .onErrorReturn("fallback")
+        var mono = getProductName()
+                .timeout(Duration.ofSeconds(1), fallback());
+
+        mono
+//                .timeout(Duration.ofMillis(200))
                 .subscribe(Util.subscriber());
 
         Util.sleepSeconds(4);
     }
 
-    private static Mono<String> getProductName(){
-        return Mono.fromSupplier(()-> "service -> " + Util.faker().commerce().productName())
-                .delayElement(Duration.ofSeconds(3));
+    private static Mono<String> getProductName() {
+        return Mono.fromSupplier(() -> "service -> " + Util.faker().commerce().productName())
+                .delayElement(Duration.ofMillis(1900));
     }
 
-    private static Mono<String> fallback(){
-        return Mono.fromSupplier(()-> "fallback -> " + Util.faker().commerce().productName())
+    private static Mono<String> fallback() {
+        return Mono.fromSupplier(() -> "fallback -> " + Util.faker().commerce().productName())
                 .delayElement(Duration.ofMillis(300))
-                .doFirst(() -> System.out.println("do first"));
+                .doFirst(() -> log.info("do first"));
     }
 }
---
+++ src/main/java/com/reactive/sec05/Lec10Transform.java
@@ -0,0 +1,52 @@
+package com.reactive.sec05;
+
+import com.github.javafaker.Faker;
+import com.reactive.common.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+public class Lec10Transform {
+
+    private static final Logger log = LoggerFactory.getLogger(Lec10Transform.class);
+
+    record Customer(int it, String name) {
+    }
+
+    record PurchaseOrder(String productName, int price, int quantity) {
+    }
+
+    public static void main(String[] args) {
+
+        var isDebugEnabled = false;
+
+        getCustomers()
+                .transform(isDebugEnabled ? addDebugger() : Function.identity())
+                .subscribe();
+
+        getPurchaseOrders()
+                .transform(isDebugEnabled ? addDebugger() : Function.identity())
+                .subscribe();
+
+    }
+
+    private static Flux<Customer> getCustomers() {
+        return Flux.range(1, 3)
+                .map(i -> new Customer(i, Util.faker().name().firstName()));
+    }
+
+    private static Flux<PurchaseOrder> getPurchaseOrders() {
+        return Flux.range(1, 5)
+                .map(i -> new PurchaseOrder(Util.faker().commerce().productName(), i, i * 10));
+    }
+
+    private static <T> UnaryOperator<Flux<T>> addDebugger() {
+        return flux -> flux
+                .doOnNext(i -> log.info("received: {}", i))
+                .doOnComplete(() -> log.info("completed"))
+                .doOnError(e -> log.error("error", e));
+    }
+}
