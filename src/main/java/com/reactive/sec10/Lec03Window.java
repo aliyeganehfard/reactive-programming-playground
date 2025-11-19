10fb9338e3f30e4ac5978c43ab0154576344c867
yeganefard <aliyeganefard@gamil.com>
2025-11-19 22:23:49 +0330

    sec10

---
+++ src/main/java/com/reactive/sec10/Lec03Window.java
@@ -0,0 +1,31 @@
+package com.reactive.sec10;
+
+import com.reactive.common.Util;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+public class Lec03Window {
+
+    public static void main(String[] args) {
+
+        eventStream()
+                .window(5)
+                .flatMap(Lec03Window::processEvents)
+                .subscribe();
+
+        Util.sleepSeconds(60);
+    }
+
+    private static Flux<String> eventStream(){
+        return Flux.interval(Duration.ofMillis(200))
+                .map(i -> "Event " + i);
+    }
+
+    private static Mono<Void> processEvents(Flux<String> flux){
+        return flux.doOnNext(e -> System.out.print("*"))
+                .doOnComplete(System.out::println)
+                .then();
+    }
+}
